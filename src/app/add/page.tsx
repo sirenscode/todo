0e2f66a12119356743597bc4179d9e4c8678775d
sirenscode <sirenst80@gmail.com>
2024-03-20 21:49:17 +0300

    Last commit Today....🥱

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -14,16 +14,20 @@ const Add: React.FC = () =>{
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const [task, setTask] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    
     async function addTask(){
-        await setDoc(docRef,{
-            title:title,
-            category:"school",
-            description:description,
-            school: true,
-            id: GlobalID,
-        });
-        window.location.reload();
-        
+        setIsLoading(true);
+        if(description!=="" && title!==""){
+            await setDoc(docRef,{
+                title:title,
+                category:"school",
+                description:description,
+                school: true,
+                id: GlobalID,
+            });
+            window.location.href="/school";
+        }
     }
 
     const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
@@ -36,19 +40,25 @@ const Add: React.FC = () =>{
     }
 
    return(
-        <div className="w-full flex flex-col items-center justify-center h-[50vh]">
+        <div className="w-full flex flex-col items-center justify-center min-h-[60vh]">
             <div className="w-3/4 flex flex-col items-center justify-center">
                 <div className="w-1/2 flex flex-col p-3">
-                    <span className="">Enter title:</span>
-                    <input onChange={handleTitleChange} className="outline-none border-[1px] border-[#eee] w-full p-1 rounded-[6px] text-[.9em]" placeholder="task"/>
+                    <span className="font-['Poppins']">Enter title:</span>
+                    <input onChange={handleTitleChange} className="font-['Poppins'] outline-none border-[1px] border-[#eee] w-full p-2 rounded-[6px] text-[.9em] hover:shadow-lg" placeholder="task"/>
+                    <div className="border rounded-lg"></div>
                 </div>
                 <div className="w-1/2 flex flex-col p-3">
-                    <span className="">Enter description:</span>
-                    <input onChange={handleDescrpitonChange} className="outline-none border-[1px] border-[#eee] w-full p-1 rounded-[6px] text-[.9em]" placeholder="description"/>
+                    <span className="font-['Poppins']">Enter description:</span>
+                    <input onChange={handleDescrpitonChange} className="description font-['Poppins'] outline-none border-[1px] border-[#eee] w-full p-2 rounded-[6px] text-[.9em] hover:shadow-lg" placeholder="description"/>
+                    <div className="border rounded-lg"></div>
                 </div>
                 <div className="w-1/2 p-3">
-                    <button className="w-1/2 bg-[#9BECE1] p-1 rounded-[6px]" onClick={addTask}>ADD</button>
+                    <button className="w-1/2 bg-[#02886F] font-['Poppins'] p-1 rounded-[6px] transition duration-300 text-[#fff] shadow-[#02886F] shadow-md ease-in font-semibold hover:bg-[#4dab9a]" onClick={addTask}>ADD</button>
                 </div>
+                {isLoading && <div className="w-1/4 flex flex-col mt-[50px]">
+                    <div className="lds-ripple"><div></div><div></div></div>
+                </div>}
+                
                 
                 
             </div>
--- src/app/school/page.tsx
+++ src/app/school/page.tsx
@@ -10,6 +10,7 @@ const School: React.FC = () =>{
     const [noTasks, setNoTasks] = useState(true);
     const [currentDate, setCurrentDate] = useState("");
     const tasksRef = collection(db, "school");
+    const [isLoading, setIsLoading] = useState(false);
     
     const q = query(tasksRef, where("school","==",true));
     async function fetchAll(){
@@ -50,41 +51,45 @@ const School: React.FC = () =>{
         },[]);
     return(
         <div className="w-full flex flex-col items-center gap-[20px]">
-            <div className="flex flex-row justify-center items-center w-full">
-                <div className="flex flex-col items-center justify-center w-full">
-                    <h1 className="text-[1.5em] font-semilight font-['Poppins']">Welcome Back, Collins</h1>
-                    {currentDate && <h2 className="text-[#8e918f] text-[.9em] font-['Poppins']">{currentDate}</h2>}
-                </div>
-                
-                <div className="ml-[auto] pr-[50px]">
-                    <Link href={"/add"} className="shadow-lg shadow-[#02886F] cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 20 20"><path fill="#02886F" d="M11 9V5H9v4H5v2h4v4h2v-4h4V9zm-1 11a10 10 0 1 1 0-20a10 10 0 0 1 0 20"/></svg></Link>
-                </div>
-            </div>
-            <div className="mt-[50px]">
-                <h2 className="text-[1.2em] font-semilight">Pending</h2>
-            </div>
+            <div className="flex flex-col items-center justify-center w-full">
+                <h1 className="text-[1.5em] font-semilight font-['Poppins']">Welcome Back, Collins</h1>
+                {currentDate && <h2 className="text-[#8e918f] text-[.9em] font-['Poppins']">{currentDate}</h2>}
+            </div>  
+            <div className="mt-[10px] w-1/2">
+                <h2 className="text-[1.2em] font-semilight w-full">Pending</h2>
+            </div>          
             {schoolData && schoolData.map((data, index)=>(
-                <div key={index} className="task-wrapper w-1/2 flex flex-row p-2 rounded-lg cursor-pointer transition duration-300 ease-in shadow-sm shadow-[#9BECE1] items-center justify-center hover:bg-[#9BECE1]" style={{fontFamily:'Afacad'}}>
-                    <div className="mr-[auto] flex flex-col w-full">
-                        <div className="flex flex-row">
-                            <h1 className="text-[1.2em] font-['Poppins']" >{data.title}</h1>
-                            <span className="text-[.7em] ml-[auto] text-[#999] font-['Poppins']">{data.id}</span>
+                <div className="w-full flex flex-col">
+                    <div className="flex flex-col w-full">
+                        <div className="flex flex-row justify-center items-center w-full">
+                            {schoolData && <div className="ml-[auto] pr-[50px] absolute w-full flex flex-col">
+                                <Link href={"/add"} className="ml-[auto] drop-shadow-lg drop-shadow-[#02886F] cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 20 20"><path fill="#02886F" d="M11 9V5H9v4H5v2h4v4h2v-4h4V9zm-1 11a10 10 0 1 1 0-20a10 10 0 0 1 0 20"/></svg></Link>
+                            </div>}
                         </div>
-                        <div className="flex flex-row items-center p-1">
-                            <p className="text-[.8em] text-[#999] font-['Poppins']">{data.description}</p>
-                            <div className="ml-[auto] check hidden">
-                                <button onClick={()=>removeTask(data.id)}><svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" viewBox="0 0 15 15"><path fill="#BA0021" fill-rule="evenodd" d="M0 7.5a7.5 7.5 0 1 1 15 0a7.5 7.5 0 0 1-15 0m7.072 3.21l4.318-5.398l-.78-.624l-3.682 4.601L4.32 7.116l-.64.768z" clip-rule="evenodd"/></svg></button>
+                        <div className="w-full flex flex-col items-center gap-[20px]">
+                            <div key={index} className="task-wrapper w-1/2 flex flex-row p-2 rounded-lg cursor-pointer transition duration-300 ease-in shadow-sm shadow-[#9BECE1] items-center justify-center hover:bg-[#9BECE1]" style={{fontFamily:'Afacad'}}>
+                                <div className="mr-[auto] flex flex-col w-full">
+                                    <div className="flex flex-row">
+                                        <h1 className="text-[1.2em] font-['Poppins']" >{data.title}</h1>
+                                        <span className="text-[.7em] ml-[auto] text-[#999] font-['Poppins']">{data.id}</span>
+                                    </div>
+                                    <div className="flex flex-row items-center p-1">
+                                        <p className="text-[.8em] text-[#999] font-['Poppins']">{data.description}</p>
+                                        <div className="ml-[auto] check hidden">
+                                            <button onClick={()=>removeTask(data.id)}><svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" viewBox="0 0 15 15"><path fill="#BA0021" fill-rule="evenodd" d="M0 7.5a7.5 7.5 0 1 1 15 0a7.5 7.5 0 0 1-15 0m7.072 3.21l4.318-5.398l-.78-.624l-3.682 4.601L4.32 7.116l-.64.768z" clip-rule="evenodd"/></svg></button>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    
                 </div>
             ))}
-            {noTasks && <div className="flex flex-col items-center">
-                <img src="/images/404.svg" className="w-full h-full"/>
-                <div className="flex flex-col gap-[10px] items-center mt-[50px]">
+            {noTasks && <div className="flex flex-col items-center w-full">
+                <img src="/images/404.svg" className="w-1/4 h-1/4"/>
+                <div className="flex flex-col gap-[10px] items-center mt-[50px] w-full justify-center">
                     <p className="font-['Poppins']">No Tasks Found</p>
-                    <Link href={"/add"} className="p-2 bg-[#4DAB9A] rounded-[10px] w-[150px] items-center flex flex-col transition duration-500 text-[#fff] ease-in hover:bg-[#02886F]">Add Task</Link>
+                    <Link href={"/add"} className="p-2 bg-[#4DAB9A] rounded-[10px] w-[150px] items-center flex flex-col transition duration-300  text-[#fff] ease-in hover:bg-[#02886F] hover:shadow-md hover:shadow-[#8e918f]">Add Task</Link>
                 </div>
                 </div>}
         </div>
